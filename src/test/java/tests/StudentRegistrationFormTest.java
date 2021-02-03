4eab6ae5a8999540180edc19a617df0dd8749cbe
Sergey Rybakin <sergey.rybakin@gmail.com>
2021-02-03 21:49:22 +0700

    add select Gender

--- src/test/java/tests/StudentRegistrationFormTest.java
+++ src/test/java/tests/StudentRegistrationFormTest.java
@@ -4,16 +4,33 @@ import org.junit.jupiter.api.Test;
 
 import com.codeborne.selenide.Condition;
 
-import static com.codeborne.selenide.Selectors.*;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byValue;
+import static com.codeborne.selenide.Selenide.*;
 
 public class StudentRegistrationFormTest
 {
     @Test
     void dataAppearsInModalPopUpWindow() {
-        open("https://demoqa.com/automation-practice-form");
-        $(".main-header").shouldHave(Condition.text("Practice Form")).should(Condition.appear);
+        String firstName = "Michail";
+        String lastName = "Lomonosov";
+        String email = "m.lomonosov@mail.ru";
+        String gender = "Male";
+        String phone = "1234567890";
 
+        open("https://demoqa.com/automation-practice-form");
+        $(".main-header").shouldHave(text("Practice Form")).should(Condition.appear);
+
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        $(byValue(gender)).sendKeys("/t ");
+
+        sleep(1000);
+//        $("#currentAddress").setValue("currentAddress here");
+//        $("#permanentAddress").setValue("permanentAddress here");
+//        $("#submit").click();
+//
+//        $("#output").shouldHave(text(name), text("aa@aa.aa"));
     }
 }
