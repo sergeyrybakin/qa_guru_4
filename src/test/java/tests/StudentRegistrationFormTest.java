2448e6c024e19dd61c1d5d9851d6c0c1f9a27e02
Sergey Rybakin <sergey.rybakin@gmail.com>
2021-02-04 11:14:11 +0700

    add verification of dateOfBirth

--- src/test/java/tests/StudentRegistrationFormTest.java
+++ src/test/java/tests/StudentRegistrationFormTest.java
@@ -1,5 +1,12 @@
 package tests;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Locale;
+
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
@@ -13,13 +20,16 @@ import static com.codeborne.selenide.Selenide.*;
 public class StudentRegistrationFormTest
 {
     @Test
-    void dataAppearsInModalPopUpWindow() {
+    void dataAppearsInModalPopUpWindow() throws ParseException
+    {
         String firstName = "Michail";
         String lastName = "theBear";
         String email = "m.thebear@mail.ru";
         String gender = "Male";
         String phone = "1234567890";
         String dateOfBirth = "19 Dec 1961";
+        String address = "Forrest str, 5";
+        String photoFileName = "1518521058110646316.jpg";
 
         open("https://demoqa.com/automation-practice-form");
         $(".main-header").shouldHave(text("Practice Form")).should(Condition.appear);
@@ -41,7 +51,10 @@ public class StudentRegistrationFormTest
         $("#hobbiesWrapper").findElement(byText("Reading")).click();
         $("#hobbiesWrapper").findElement(byText("Music")).click();
 
-        $("#uploadPicture").uploadFromClasspath("1518521058110646316.jpg");
+        $("#uploadPicture").uploadFromClasspath(photoFileName);
+
+        $("#currentAddress").scrollTo();
+        $("#currentAddress").setValue(address);
 
         $("#submit").scrollTo();
         $("#submit").click();
@@ -49,7 +62,21 @@ public class StudentRegistrationFormTest
         sleep(1000);
 
         $(".modal-content").should(appear);
-//
-//        $("#output").shouldHave(text(name), text("aa@aa.aa"));
+        $(".modal-body").shouldHave(text(firstName));
+        $(".modal-body").shouldHave(text(lastName));
+        $(".modal-body").shouldHave(text(email));
+        $(".modal-body").shouldHave(text(gender));
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy", Locale.ENGLISH);
+        LocalDate dateTime = LocalDate.parse(dateOfBirth, formatter);
+        DateTimeFormatter formatterOutput = DateTimeFormatter.ofPattern("dd MMMM,yyyy", Locale.ENGLISH);
+        System.out.println("###################3 DATE:" + dateTime.format(formatterOutput));
+        String hindiDate = dateTime.format(formatterOutput);
+        System.out.println("###################4 DATE:" + hindiDate);
+        $(".modal-body").shouldHave(text(hindiDate));
+        $(".modal-body").shouldHave(text(phone));
+        $(".modal-body").shouldHave(text(photoFileName));
+        $(".modal-body").shouldHave(text(address));
+
     }
 }
