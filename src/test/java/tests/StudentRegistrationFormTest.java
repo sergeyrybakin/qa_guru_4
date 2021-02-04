90792ebfc1ae129b8ff82b24067838b5ff042c1c
Sergey Rybakin <sergey.rybakin@gmail.com>
2021-02-04 11:45:18 +0700

    add hobby as String Array and verification of hobby

--- src/test/java/tests/StudentRegistrationFormTest.java
+++ src/test/java/tests/StudentRegistrationFormTest.java
@@ -1,10 +1,7 @@
 package tests;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.Locale;
 
 import org.junit.jupiter.api.Test;
@@ -20,7 +17,7 @@ import static com.codeborne.selenide.Selenide.*;
 public class StudentRegistrationFormTest
 {
     @Test
-    void dataAppearsInModalPopUpWindow() throws ParseException
+    void dataAppearsInModalPopUpWindow()
     {
         String firstName = "Michail";
         String lastName = "theBear";
@@ -30,6 +27,7 @@ public class StudentRegistrationFormTest
         String dateOfBirth = "19 Dec 1961";
         String address = "Forrest str, 5";
         String photoFileName = "1518521058110646316.jpg";
+        String[] stringArray = new String[] { "Reading", "Music" };
 
         open("https://demoqa.com/automation-practice-form");
         $(".main-header").shouldHave(text("Practice Form")).should(Condition.appear);
@@ -39,7 +37,7 @@ public class StudentRegistrationFormTest
         $("#userEmail").setValue(email);
         $(byValue(gender)).sendKeys("/t ");
         $("#userNumber").setValue(phone);
-
+        //Date of Birth
         $("#dateOfBirthInput").click();
         $("#dateOfBirthInput").sendKeys(dateOfBirth);
         for(int i=0; i<11; i++)
@@ -47,9 +45,11 @@ public class StudentRegistrationFormTest
         for(int i=0; i<11; i++)
             $("#dateOfBirthInput").sendKeys(Keys.BACK_SPACE);
         $("#dateOfBirthInput").pressEnter();
-
-        $("#hobbiesWrapper").findElement(byText("Reading")).click();
-        $("#hobbiesWrapper").findElement(byText("Music")).click();
+        //Hobby
+        for (String s : stringArray)
+        {
+            $("#hobbiesWrapper").findElement(byText(s)).click();
+        }
 
         $("#uploadPicture").uploadFromClasspath(photoFileName);
 
@@ -61,22 +61,29 @@ public class StudentRegistrationFormTest
 
         sleep(1000);
 
+        //Verifications
         $(".modal-content").should(appear);
         $(".modal-body").shouldHave(text(firstName));
         $(".modal-body").shouldHave(text(lastName));
         $(".modal-body").shouldHave(text(email));
         $(".modal-body").shouldHave(text(gender));
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy", Locale.ENGLISH);
-        LocalDate dateTime = LocalDate.parse(dateOfBirth, formatter);
-        DateTimeFormatter formatterOutput = DateTimeFormatter.ofPattern("dd MMMM,yyyy", Locale.ENGLISH);
-        System.out.println("###################3 DATE:" + dateTime.format(formatterOutput));
-        String hindiDate = dateTime.format(formatterOutput);
-        System.out.println("###################4 DATE:" + hindiDate);
-        $(".modal-body").shouldHave(text(hindiDate));
+        $(".modal-body").shouldHave(text(getHindiDate(dateOfBirth)));
         $(".modal-body").shouldHave(text(phone));
+        for (String s : stringArray)
+        {
+            $(".modal-body").shouldHave(text(s));
+        }
         $(".modal-body").shouldHave(text(photoFileName));
         $(".modal-body").shouldHave(text(address));
 
+    }
+
+    private String getHindiDate(String dateOfBirth)
+    {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy", Locale.ENGLISH);
+        LocalDate dateTime = LocalDate.parse(dateOfBirth, formatter);
+        DateTimeFormatter formatterOutput = DateTimeFormatter.ofPattern("dd MMMM,yyyy", Locale.ENGLISH);
+        String hindiDate = dateTime.format(formatterOutput);
+        return hindiDate;
     }
 }
