1b19ee8fed2734a9e0e30ffb76bd5a402afc802c
Sergey Rybakin <sergey.rybakin@gmail.com>
2021-02-04 20:52:34 +0700

    add selections, click <Close modal window>

--- src/test/java/tests/StudentRegistrationFormTest.java
+++ src/test/java/tests/StudentRegistrationFormTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
@@ -25,9 +26,11 @@ public class StudentRegistrationFormTest
         String gender = "Male";
         String phone = "1234567890";
         String dateOfBirth = "19 Dec 1961";
-        String address = "Forrest str, 5";
         String photoFileName = "1518521058110646316.jpg";
         String[] stringArray = new String[] { "Reading", "Music" };
+        String address = "Forrest str, 5, 534533";
+        String state = "Haryana";
+        String city = "Panipat";
 
         open("https://demoqa.com/automation-practice-form");
         $(".main-header").shouldHave(text("Practice Form")).should(Condition.appear);
@@ -38,13 +41,7 @@ public class StudentRegistrationFormTest
         $(byValue(gender)).sendKeys("/t ");
         $("#userNumber").setValue(phone);
         //Date of Birth
-        $("#dateOfBirthInput").click();
-        $("#dateOfBirthInput").sendKeys(dateOfBirth);
-        for(int i=0; i<11; i++)
-            $("#dateOfBirthInput").sendKeys(Keys.LEFT);
-        for(int i=0; i<11; i++)
-            $("#dateOfBirthInput").sendKeys(Keys.BACK_SPACE);
-        $("#dateOfBirthInput").pressEnter();
+        typeDateOfBirth(dateOfBirth);
         //Hobby
         for (String s : stringArray)
         {
@@ -53,13 +50,14 @@ public class StudentRegistrationFormTest
 
         $("#uploadPicture").uploadFromClasspath(photoFileName);
 
+        //Address
         $("#currentAddress").scrollTo();
         $("#currentAddress").setValue(address);
+        selectInDropDownList("state", state);
+        selectInDropDownList("city", city);
 
-        $("#submit").scrollTo();
-        $("#submit").click();
-
-        sleep(1000);
+        //Submit form
+        $("#submit").scrollTo().click();
 
         //Verifications
         $(".modal-content").should(appear);
@@ -75,7 +73,28 @@ public class StudentRegistrationFormTest
         }
         $(".modal-body").shouldHave(text(photoFileName));
         $(".modal-body").shouldHave(text(address));
+        $(".modal-body").shouldHave(text(state+" "+city));
 
+        $("#closeLargeModal").scrollTo().click();
+    }
+
+    private void typeDateOfBirth(String dateOfBirth)
+    {
+        $("#dateOfBirthInput").click();
+        $("#dateOfBirthInput").sendKeys(dateOfBirth);
+        for(int i=0; i<11; i++)
+            $("#dateOfBirthInput").sendKeys(Keys.LEFT);
+        for(int i=0; i<11; i++)
+            $("#dateOfBirthInput").sendKeys(Keys.BACK_SPACE);
+        $("#dateOfBirthInput").pressEnter();
+    }
+
+    private void selectInDropDownList(String type, String name)
+    {
+        SelenideElement selectState = $("#" + type + " div[class $= '-placeholder']");
+        selectState.scrollTo().click();
+        SelenideElement dropDownStateList = $("div[class $= '-menu']").should(appear);
+        dropDownStateList.findElement(byText(name)).click();
     }
 
     private String getHindiDate(String dateOfBirth)
@@ -83,7 +102,6 @@ public class StudentRegistrationFormTest
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy", Locale.ENGLISH);
         LocalDate dateTime = LocalDate.parse(dateOfBirth, formatter);
         DateTimeFormatter formatterOutput = DateTimeFormatter.ofPattern("dd MMMM,yyyy", Locale.ENGLISH);
-        String hindiDate = dateTime.format(formatterOutput);
-        return hindiDate;
+        return dateTime.format(formatterOutput);
     }
 }
